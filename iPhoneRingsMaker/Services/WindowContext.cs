498f6cb10e53792ad6c407fde95702df14974827
Mickaël Véril <mika.veril@wanadoo.fr>
2026-09-20 15:07:40 +0200

    Restore redirected project activations to foreground

--- iPhoneRingsMaker/Services/WindowContext.cs
+++ iPhoneRingsMaker/Services/WindowContext.cs
@@ -3,6 +3,10 @@ using iPhoneRingsMaker.Contracts.Services;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+
 namespace iPhoneRingsMaker.Services;
 
 public sealed class WindowContext : IWindowContext
@@ -26,7 +30,14 @@ public sealed class WindowContext : IWindowContext
         set => _window.AppTitleBar = value;
     }
 
-    public void Activate() => _window.Activate();
+    public void Activate()
+    {
+        _window.Activate();
+
+        var hwnd = new HWND(WinRT.Interop.WindowNative.GetWindowHandle(_window));
+        PInvoke.ShowWindow(hwnd, SHOW_WINDOW_CMD.SW_RESTORE);
+        PInvoke.SetForegroundWindow(hwnd);
+    }
 
     public void CloseWithoutConfirmation() => _window.CloseWithoutConfirmation();
 }
